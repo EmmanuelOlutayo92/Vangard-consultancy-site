af9fbbfbcedf742573837ae7a431279e76c975c9
Emmanuel Olutayo <emmanuelolutayo92@gmail.com>
2026-02-06 10:39:28 +0000

    Fix component imports and CSS path

--- app/app.vue
+++ app/app.vue
@@ -15,3 +15,15 @@
     <SiteFooter />
   </div>
 </template>
+
+<script setup>
+import HeaderNav from '../components/HeaderNav.vue'
+import HeroSection from '../components/HeroSection.vue'
+import FounderSection from '../components/FounderSection.vue'
+import ServicesSection from '../components/ServicesSection.vue'
+import ProjectsSection from '../components/ProjectsSection.vue'
+import TestimonialsSection from '../components/TestimonialsSection.vue'
+import CtaSection from '../components/CtaSection.vue'
+import ContactSection from '../components/ContactSection.vue'
+import SiteFooter from '../components/SiteFooter.vue'
+</script>
