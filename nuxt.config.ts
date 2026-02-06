edd9252263ada6aa79333341da98a17f38192c86
Emmanuel Olutayo <emmanuelolutayo92@gmail.com>
2026-02-06 10:58:59 +0000

    Ensure home route is prerendered

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  nitro: {
+    prerender: {
+      routes: ['/']
+    }
+  },
   app: {
     head: {
       htmlAttrs: { lang: 'en' },
